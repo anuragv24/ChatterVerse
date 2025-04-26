b889343da8c4c577288bd7987b179d4082c7a7fd
Anurag Verma <anuragv984@gmail.com>
2025-04-26 12:42:20 +0530

    Added logout

--- ChatterVerse/src/Pages/Room.jsx
+++ ChatterVerse/src/Pages/Room.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { ID, Query } from "appwrite";
-import client, { COLLECTION_ID_MESSAGE, DATABASE_ID, databases, } from "../appwriteConfig";
-import  {Trash2} from 'react-feather'
+import client, {
+  COLLECTION_ID_MESSAGE,
+  DATABASE_ID,
+  databases,
+} from "../appwriteConfig";
+import { Trash2 } from "react-feather";
+import Header from "../compoents/Header";
 
 const Room = () => {
   const [message, setMessage] = useState([]);
@@ -12,15 +17,12 @@ const Room = () => {
       const response = await databases.listDocuments(
         DATABASE_ID,
         COLLECTION_ID_MESSAGE,
-        [
-          Query.orderDesc("$createdAt"), 
-          Query.limit(5)
-        ]
+        [Query.orderDesc("$createdAt"), Query.limit(5)]
       );
       console.log("Response:", response);
       setMessage(response.documents);
     } catch (error) {
-      console.log("Error fetching messages:", error)
+      console.log("Error fetching messages:", error);
     }
   };
 
@@ -43,33 +45,52 @@ const Room = () => {
   };
 
   const deleteMessage = async (message_id) => {
-   await databases.deleteDocument(DATABASE_ID, COLLECTION_ID_MESSAGE, message_id)
+    await databases.deleteDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGE,
+      message_id
+    );
     // setMessage(prev => prev.filter(msg => msg.$id !== message_id))
-  }
- 
+  };
+
   useEffect(() => {
     getMessages();
-    const unsubscribe = client.subscribe(`databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGE}.documents`, response => {
-      console.log("Real time",response)
+    const unsubscribe = client.subscribe(
+      `databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGE}.documents`,
+      (response) => {
+        console.log("Real time", response);
 
-      if(response.events.includes("databases.*.collections.*.documents.*.create")){
-        setMessage((prevMsg) => [response.payload, ...prevMsg])
-        console.log("A msg is created")
-      }
+        if (
+          response.events.includes(
+            "databases.*.collections.*.documents.*.create"
+          )
+        ) {
+          setMessage((prevMsg) => [response.payload, ...prevMsg]);
+          console.log("A msg is created");
+        }
 
-      if(response.events.includes("databases.*.collections.*.documents.*.delete")){
-        setMessage(prev => prev.filter(msg => msg.$id !== response.payload.$id))
-        console.log("A msg is delete")
+        if (
+          response.events.includes(
+            "databases.*.collections.*.documents.*.delete"
+          )
+        ) {
+          setMessage((prev) =>
+            prev.filter((msg) => msg.$id !== response.payload.$id)
+          );
+          console.log("A msg is delete");
+        }
       }
-    })
+    );
     return () => {
       unsubscribe();
-    }
+    };
   }, []);
 
   return (
     <main className="container">
+       <Header/>
       <div className="room--container">
+       
         <form onSubmit={handleSubmit} id="message--form">
           <div>
             <textarea
@@ -92,10 +113,14 @@ const Room = () => {
             <div className="message--wrapper" key={message.$id}>
               <div className="message--header">
                 <small className="message-timestamp">
-                 {new Date(message.$createdAt).toLocaleString()}
+                  {new Date(message.$createdAt).toLocaleString()}
                 </small>
-                <Trash2  className="delete--btn" onClick={() => {deleteMessage(message.$id)}}/>
-                
+                <Trash2
+                  className="delete--btn"
+                  onClick={() => {
+                    deleteMessage(message.$id);
+                  }}
+                />
               </div>
               <div className="message--body">
                 <span>{message.body}</span>
--- ChatterVerse/src/utils/AuthContext.jsx
+++ ChatterVerse/src/utils/AuthContext.jsx
@@ -1,54 +1,75 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { createContext } from 'react'
-import { account } from '../appwriteConfig'
-import {useNavigate} from 'react-router'
+import React, { useContext, useEffect, useState } from "react";
+import { createContext } from "react";
+import { account } from "../appwriteConfig";
+import { useNavigate } from "react-router";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true)
-    const navigate = useNavigate()
-    
+  useEffect(() => {
+    getUserOnLoad()
+  }, []);
 
-    useEffect(() => {
-        setLoading(false)
-    }, [])
-
-    const handleUserLogin = async (e, credentials) => {
-        e.preventDefault()
-        
-        try {
-            console.log("cred",credentials);
-            
-            const response = await account.createEmailPasswordSession(credentials.email, credentials.password)
-
-            console.log("Logged In:", response);
-
-            const accountDetail =await account.get()
-            setUser(accountDetail)
-
-            navigate('/')
-            
-        } catch (error) {
-            console.log("error while login:", error);
-            
-        }
+  const getUserOnLoad = async () => {
+    try {
+      const accountDetail = await account.get();
+      setUser(accountDetail);
+    } catch (error) {
+      console.error(error);
     }
+    setLoading(false)
+  };
 
-    const contextData = {
-        user,
-        handleUserLogin
+  const handleUserLogin = async (e, credentials) => {
+    e.preventDefault();
+
+    try {
+      console.log("cred", credentials);
+
+      const response = await account.createEmailPasswordSession(
+        credentials.email,
+        credentials.password
+      );
+
+      console.log("Logged In:", response);
+
+      const accountDetail = await account.get();
+      setUser(accountDetail);
+
+      navigate("/");
+    } catch (error) {
+      console.log("error while login:", error);
     }
+  };
 
-    return <AuthContext.Provider value={contextData}>
-        {loading ? <p>Loading .....</p> : children}
+  const handleUserLogout = async () => {
+    try {
+       await account.deleteSession('current')
+        setUser(null)
+    } catch (error) {
+        console.error(error)
+    }
+  }
+
+  const contextData = {
+    user,
+    handleUserLogin,
+    handleUserLogout,
+  };
+
+  return (
+    <AuthContext.Provider value={contextData}>
+      {loading ? <p>Loading .....</p> : children}
     </AuthContext.Provider>
-}   
+  );
+};
 
 export const useAuth = () => {
-    return useContext(AuthContext)
-}
+  return useContext(AuthContext);
+};
 
-export default AuthContext
+export default AuthContext;
